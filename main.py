dfc54ae6d2f43691f90e4ce7ba32bc1e00ff8def
Yash Dhasmana <yashdhasmana@Yashs-MacBook-Air.local>
2023-02-16 21:20:55 +0530

    dateTime functionality added
    added functionality to send a message at a particular time and date.

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 from twilio.rest import Client
 from dotenv import load_dotenv
-import os 
+import os
+import datetime
+import time
 
-load_dotenv()
+load_dotenv() # loading our auth tokens from env file 
 
-Sid = os.getenv('SID')
-auth_token = os.getenv('Auth_Token')
+Sid = os.getenv('SID')  # getting SID
+auth_token = os.getenv('Auth_Token')  # getting authToken
 
-client = Client(Sid, auth_token)
+def sendMessage(message):
+    client = Client(Sid, auth_token)
+    message = client.messages.create(
+        body=message,
+        from_=os.getenv('Sender_Number'),
+        to=os.getenv('Reciever_Number')
+    )
+    print(f"Message sent: {message.sid}")
 
-Text = "Hello World!"
+target_time = datetime.datetime(2023, 2, 16, 20, 30, 0) # yyyy, m, dd, hh, mm, ss
 
-message = client.messages.create(
-    body= Text,
-    from_=os.getenv('Sender_Number'),
-    to=os.getenv('Reciever_Number')
-)
+while datetime.datetime.now() < target_time:
+    time.sleep(1)
 
-print(message.sid)
+if datetime.datetime.now() >= target_time:
+    message = "This message should deliver at 8:30pm on February 16, 2023"
+    sendMessage(message)
+
